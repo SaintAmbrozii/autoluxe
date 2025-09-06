4d1060dc44fa2d7e7cd351b26ce7a2728b7fa2fb
Samsonov Dmitrii <samdim2011@mail.ru>
2025-09-06 16:56:33 +0700

    upd addsub dto

--- src/main/java/com/example/autoluxe/dto/UserAccountDto.java
+++ src/main/java/com/example/autoluxe/dto/UserAccountDto.java
@@ -1,7 +1,10 @@
 package com.example.autoluxe.dto;
 
 import com.example.autoluxe.domain.UserAccount;
+import com.example.autoluxe.utils.DateUtils;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class UserAccountDto {
@@ -16,12 +19,12 @@ public class UserAccountDto {
 
     private String pass;
 
-    private String expires;
+    private LocalDateTime expires;
 
     public static UserAccountDto toDto (UserAccount userAccount) {
         UserAccountDto accountDto = new UserAccountDto();
         accountDto.setId(userAccount.getId());
-        accountDto.setExpires(userAccount.getRFCExpires().toString());
+        accountDto.setExpires(userAccount.getRFCExpires());
         accountDto.setName(userAccount.getName());
         accountDto.setPass(userAccount.getPass());
         accountDto.setLogin(userAccount.getLogin());
